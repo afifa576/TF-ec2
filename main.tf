8e31b64284ab6f273fa578cc2c4f527f441ddd8e
forjenkins <afifaamara576@gmail.com>
2023-03-21 17:54:20 -0700

    added tag

--- main.tf
+++ main.tf
@@ -2,5 +2,6 @@ resource "aws_instance" "demo" {
     ami=var.ami
     instance_type=var.instance
     key_name=var.key
+    tags=var.tag
   
 }
--- vars.tf
+++ vars.tf
@@ -5,5 +5,6 @@ variable "instance"{
 
 }
 variable "key"{
-    
+
 }
+variable "tag"{}
